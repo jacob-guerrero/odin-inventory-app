c7d5be01f9733ae93029c2b99f8532a03dab9797
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-01-29 11:29:03 -0500

    Set up categories routes

---
+++ routes/categoryRouter.js
@@ -0,0 +1,35 @@
+const { Router } = require("express");
+const {
+  getAllCategories,
+  getCategoryDetails,
+  createCategoryGet,
+  createCategoryPost,
+  updateCategoryGet,
+  updateCategoryPost,
+  deleteCategory,
+} = require("../controllers/categoriesController");
+
+const categoryRouter = Router();
+
+// GET all categories
+categoryRouter.get("/", getAllCategories);
+
+// GET category details (including items in the category)
+categoryRouter.get("/:id", getCategoryDetails);
+
+// GET form to create a new category
+categoryRouter.get("/create", createCategoryGet);
+
+// POST request to create a new category
+categoryRouter.post("/create", createCategoryPost);
+
+// GET form to update an existing category
+categoryRouter.get("/edit/:id", updateCategoryGet);
+
+// POST request to update an existing category
+categoryRouter.post("/edit/:id", updateCategoryPost);
+
+// POST request to delete a category
+categoryRouter.post("/delete/:id", deleteCategory);
+
+module.exports = categoryRouter;
